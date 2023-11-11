b149a54808fd4d7df3386f582e4ca373137970fe
djdjz7 <djdjz7@icloud.com>
2023-11-11 08:48:31 +0800

    chore: i dont know waht to write this time.

---
+++ OpenFlier.Desktop/MqttService/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenFlier.Desktop.Services
+{
+    public class User
+    {
+        required public string Username { get; set; }
+        public string? UserId { get; set; }
+        public string? CurrentClientId { get; set; }
+        public bool CommandInputAllowed { get; set; }
+    }
+}
