4d40cabe214ca3e950e176d420851bc496d3b723
djdjz7 <djdjz7@icloud.com>
2024-01-17 19:22:36 +0800

    just trying wix

---
+++ GenerateWiXFiles.ps1
@@ -0,0 +1,15 @@
+﻿$files = Get-ChildItem -Path '../OpenFlierRelease'
+# $files = [System.IO.Directory]::GetFiles("../OpenFlierRelease")
+$originalContent = Get-Content -Path '.\Installer.MSI\Files.wxs.Template'
+$builder = [System.Text.StringBuilder]::new()
+foreach($file in $files)
+{
+    if(!$file.PSIsContainer)
+    {
+        [void]$builder.Append("<Component><File Source=""$($file.FullName)"" /></Component>`r`n")
+    }
+}
+$toReplace = $builder.ToString();
+$newContent = $originalContent -replace '%FILES_REPLACE_SECTION%', $toReplace
+
+$newContent | Set-Content -Path ".\Installer.MSI\Files.wxs"
