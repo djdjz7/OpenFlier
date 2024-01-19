0544fbbdbfa2090b2fb16d1977cac6e20a411297
djdjz7 <djdjz7@icloud.com>
2024-01-19 17:07:15 +0800

    yeah!

---
+++ printTime.ps1
@@ -0,0 +1,4 @@
+﻿$cst = [System.TimeZoneInfo]::FindSystemTimeZoneById("China Standard Time")
+$dateTime = [System.DateTime]::UtcNow
+$cstCurrentTime = [System.TimeZoneInfo]::ConvertTimeFromUtc($dateTime, $cst)
+$cstCurrentTime.ToString() > .\BuildArchive\build-time
