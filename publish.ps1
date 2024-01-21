13134982aa0e760642aa3e845492feed9b10bb0c
djdjz7 <djdjz7@icloud.com>
2024-01-21 15:33:03 +0800

    update build script

--- publish.ps1
+++ publish.ps1
@@ -2,7 +2,12 @@
 dotnet publish OpenFlier.Utils -p:PublishProfile=FolderProfile;
 dotnet publish OpenFlier.Updater -p:PublishProfile=FolderProfile;
 
-7z a OpenFlier.7z dist/* -r && Copy-Item "OpenFlier.7z" -Destination ".\BuildArchive\OpenFlier.7z";
+7z a OpenFlier.7z dist/* -r -ssw
+$Exist = Test-Path ".\BuildArchive\OpenFlier.7z";
+if ($Exist -eq "True") {
+    Remove-Item ".\BuildArchive\OpenFlier.7z";
+}
+Move-Item -Path "OpenFlier.7z" -Destination ".\BuildArchive\OpenFlier.7z";
 
 $cst = [System.TimeZoneInfo]::FindSystemTimeZoneById('China Standard Time')
 $dateTime = [System.DateTime]::UtcNow
