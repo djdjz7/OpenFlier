fd4040c35aff618ca3c3d03fd7f74004951056ae
Jason Dai <djdjz7@icloud.com>
2024-03-02 22:00:28 +0800

    build devutils

--- publish.ps1
+++ publish.ps1
@@ -1,13 +1,21 @@
 ﻿dotnet publish OpenFlier.Desktop -p:PublishProfile=FolderProfile;
 dotnet publish OpenFlier.Utils -p:PublishProfile=FolderProfile;
 dotnet publish OpenFlier.Updater -p:PublishProfile=FolderProfile;
+dotnet publish OpenFlier.DevUtils -p:PublishProfile=FolderProfile;
 
 7z a OpenFlier.7z dist/* -r -ssw
+7z a DevUtils.7z devutils-dist/* -r -ssw
 $Exist = Test-Path ".\BuildArchive\OpenFlier.7z";
 if ($Exist -eq "True") {
     Remove-Item ".\BuildArchive\OpenFlier.7z";
 }
 Move-Item -Path "OpenFlier.7z" -Destination ".\BuildArchive\OpenFlier.7z";
+
+$Exist = Test-Path ".\BuildArchive\DevUtils.7z";
+if ($Exist -eq "True") {
+    Remove-Item ".\BuildArchive\DevUtils.7z";
+}
+Move-Item -Path "DevUtils.7z" -Destination ".\BuildArchive\DevUtils.7z";
 
 $cst = [System.TimeZoneInfo]::FindSystemTimeZoneById('China Standard Time')
 $dateTime = [System.DateTime]::UtcNow
