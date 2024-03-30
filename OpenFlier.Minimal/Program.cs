263f9c498aa31f40680d88cdedc7b89d895de671
Jason Dai <djdjz7@icloud.com>
2024-03-30 23:06:23 +0800

    minimal example

---
+++ OpenFlier.Minimal/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenFlier.Core;
+using OpenFlier.Core.Services;
+namespace OpenFlier.Minimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceManager = new ServiceManager(new CoreConfig());
+            serviceManager.OnLoadCompleted += ServiceManager_OnLoadCompleted;
+            serviceManager.BeginLoad();
+            while (true)
+            { }
+        }
+
+        private static void ServiceManager_OnLoadCompleted(bool isReloaded)
+        {
+            Console.WriteLine($"IP Address:   {CoreStorage.IPAddresses.Select(x => x.ToString())}");
+            Console.WriteLine($"Connect code: {CoreStorage.ConnectCode}");
+            Console.WriteLine($"Machine Id.:  {CoreStorage.MachineIdentifier}");
+        }
+    }
+}
